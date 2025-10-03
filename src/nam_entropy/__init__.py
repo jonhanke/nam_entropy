4336a3b99cfd967b54c4e20c4e957b3b323afb74
Jonathan Hanke <jonhanke@princeton.edu>
2025-10-03 09:57:18 -0400

    Better structuring / documentation of __init__.py imports.

--- src/nam_entropy/__init__.py
+++ src/nam_entropy/__init__.py
@@ -1,9 +1,17 @@
 
-## Import all classes and packages relevant for the demo
+## Imports for entropy calculation and demo notebooks:
+## ---------------------------------------------------
+
+## Import data creation / preparation routines
 from .make_data import make_samples_dataframe_from_distributions
-from .integrated_distribution_2d_sampler import Integrated2DDistributionWidget, Distribution2DSampler
 from .data_prep import data_df_to_pytorch_data_tensors_and_labels, \
                        prepare_labeled_tensor_dataset, \
                        convert_tensor_list_to_dataframe
+
+## Import visualization routines
+from .integrated_distribution_2d_sampler import Integrated2DDistributionWidget, Distribution2DSampler
 from .bin_distribution_plots import plot_tensor_bars
+
+
+## Import main entropy calculation routines
 from .h import *
